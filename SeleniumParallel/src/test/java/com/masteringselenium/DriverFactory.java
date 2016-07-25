88e87219981fab64b341e7cf22a1573029d2a475
Saul Wang <saulwang@Objectivasoftware.com>
2016-07-25 17:29:57 +0800

    Add selenium and testng parallel test.

---
+++ SeleniumParallel/src/test/java/com/masteringselenium/DriverFactory.java
@@ -0,0 +1,34 @@
+package com.masteringselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeSuite;
+
+/**
+ * Created by saulwang on 7/25/2016.
+ */
+public class DriverFactory {
+
+    private static ThreadLocal<WebDriverThread> driverThread;
+
+    @BeforeSuite
+    public static void instantiateDriverObject(){
+        driverThread = new ThreadLocal<WebDriverThread>(){
+            @Override
+            protected WebDriverThread initialValue(){
+                WebDriverThread webDriverThread = new WebDriverThread();
+                return webDriverThread;
+            }
+        };
+    }
+
+    public static WebDriver getDriver() throws Exception{
+        return driverThread.get().getDriver();
+    }
+
+    @AfterMethod
+    public static void quitDriver() throws Exception {
+        driverThread.get().quitDriver();
+    }
+
+}

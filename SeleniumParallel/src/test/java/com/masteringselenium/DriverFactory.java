ef60ad3b085aded43c38fb9d0ab0f62572374d75
Saul Wang <saulwang@Objectivasoftware.com>
2016-07-27 13:46:46 +0800

    Add webDriver thread pool.

--- SeleniumParallel/src/test/java/com/masteringselenium/DriverFactory.java
+++ SeleniumParallel/src/test/java/com/masteringselenium/DriverFactory.java
@@ -2,7 +2,12 @@ package com.masteringselenium;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by saulwang on 7/25/2016.
@@ -11,12 +16,16 @@ public class DriverFactory {
 
     private static ThreadLocal<WebDriverThread> driverThread;
 
+    private static List<WebDriverThread> webDriverThreadPool =
+            Collections.synchronizedList(new ArrayList<WebDriverThread>());
+
     @BeforeSuite
     public static void instantiateDriverObject(){
         driverThread = new ThreadLocal<WebDriverThread>(){
             @Override
             protected WebDriverThread initialValue(){
                 WebDriverThread webDriverThread = new WebDriverThread();
+                webDriverThreadPool.add(webDriverThread);
                 return webDriverThread;
             }
         };
@@ -26,14 +35,21 @@ public class DriverFactory {
         return driverThread.get().getDriver();
     }
 
-    @AfterMethod
-    public static void quitDriver() throws Exception {
-        driverThread.get().quitDriver();
-    }
+//    @AfterSuite
+//    public static void quitDriver() throws Exception {
+//        driverThread.get().quitDriver();
+//    }
 
     @AfterMethod
     public static void clearCookies() throws Exception{
-        getDriver().manage().deleteAllCookies();
+        getDriver().manage().deleteAllCookies();;
+    }
+
+    @AfterSuite
+    public static void closeDriverObjects(){
+        for (WebDriverThread webDriverThread : webDriverThreadPool) {
+            webDriverThread.quitDriver();
+        }
     }
 
 }
